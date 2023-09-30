7fbfb1dc734b0f3f8fcb3016225077cc0b75fe19
Víctor Adán <vctradn@gmail.com>
2023-09-29 22:18:11 -0500

    Added pre & post to etude

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,6 @@
 
 module Main where
 
--- import qualified Data.ByteString.Lazy as BS (concat, putStr, ByteString)
-
 import Data.Binary (encode)
 import qualified Data.ByteString.Lazy as BS (ByteString, concat, putStr)
 import qualified GHC.Int
@@ -14,25 +12,16 @@ import Music1Bit.Types as C
 toInt :: Tick -> GHC.Int.Int32
 toInt x = if x then 1 else 0
 
--- music = [ C.mix [C.impulse 102, C.impulse 100, C.impulse 101, C.impulse 99] sample | sample <- [0..100000]]
--- music = [ C.mix (map (C.impulse . (*80)) [1, 3..50]) sample | sample <- [0..1000000]]
--- music = [ C.mix (map C.impulse [89..103]) sample | sample <- [0..1000000]]
--- music = [ C.mix (map C.impulse [1000..1010]) sample | sample <- [0..1000000]]
+-- | polyrhythmic clusters
+pre1 = C.ioi2signal $ reverse [10, 200 .. 12002]
+pre2 = C.ioi2signal $ reverse [12, 200 .. 12000]
+pre3 = C.ioi2signal $ reverse [11, 200 .. 12001]
+pre = C.mix [pre1, pre2, pre3]
 
--- music = [ C.mix (map C.impulse [400..410]) sample | sample <- [0..100000]]
--- music = [ C.seq  (C.impulse 1000, 15000) (C.mix (map C.impulse [400..410])) sample | sample <- [0..1000000]]
-
--- Best
--- phase = product [400..401] `div` 2
--- m1 = C.mix (map (C.shift phase . C.impulse) [400..413])
--- pre = C.impulse 400
--- pre = C.ioi2signal $ reverse [80..1440]
--- music = [ C.seq  (pre, 90000)  m1 n | n <- [0..5000000]]
--- music = [ C.seq  (C.impulse 1000, 15000) (C.mix (map (C.shift (4007*5409 `div` 2) . C.impulse) [4000, 4100..5003])) sample | sample <- [0..1000000]]
-
--- | polyrhythmic clusters:
-pre = C.ioi2signal $ reverse [10, 200 .. 10000]
-
+post1 = C.ioi2signal [11, 200 .. 12002]
+post2 = C.ioi2signal [13, 200 .. 12008]
+post3 = C.ioi2signal [12, 200 .. 12011]
+post = C.mix [post1, post2, post3]
 phMul = 100
 
 c1 = C.shift (3000 * phMul) $ C.mix $ map C.impulse [3000 .. 3020]
@@ -43,9 +32,9 @@ c3 = C.shift (7000 * phMul) $ C.mix $ map C.impulse [7000, 7002 .. 7070]
 
 c4 = C.shift (20000 * phMul) $ C.mix $ map C.impulse [20000, 20003 .. 20200]
 
-call = C.seq (pre, 260000) (C.mix [c1, c2, c3, c4])
+call = C.seq (pre, 380000) (C.mix [c1, c2, c3, c4])
 
-music = map call [0 .. 10000000]
+music = map call [0 .. 20000000]
 
 main :: IO ()
 -- main = BS.putStr $ BS.concat $ map (encode . (*2^14) . Main.toInt) music

f69d528649c7ed540454f0e5b9456a5b8f34b557
Víctor Adán <vctradn@gmail.com>
2024-05-11 20:25:44 -0500

    Added Phasor, fixed collapse

--- compositions/tepoz_vi/Main.hs
+++ compositions/tepoz_vi/Main.hs
@@ -10,19 +10,20 @@ import qualified GHC.Base as M
 post1 = M.sequential $ map M.imp [11, 200 .. 12002]
 post2 = M.sequential $ map M.imp [13, 200 .. 12008]
 post3 = M.sequential $ map M.imp [12, 200 .. 12011]
-post = post1 :=: post2 :=: post3
+post = M.parallel [post1, post2, post3]
 
 -- phMul = 100
--- c1 = M.shift (3000 * phMul) $ M.mix $ map M.impulse [3000 .. 3020]
--- c2 = M.shift (5000 * phMul) $ M.mix $ map M.impulse [5000 .. 5050]
--- c3 = M.shift (7000 * phMul) $ M.mix $ map M.impulse [7000, 7002 .. 7070]
--- c4 = M.shift (19000 * phMul) $ M.mix $ map M.impulse [19000, 19003 .. 19200]
-
+count = 1000000
+c1 = M.parallel $ map (M.phasor count . (: [])) [3000 .. 3020]
+c2 = M.parallel $ map (M.phasor count . (: [])) [5000 .. 5050]
+c3 = M.parallel $ map (M.phasor count . (: [])) [7000, 7002 .. 7070]
+c4 = M.parallel $ map (M.phasor count . (: [])) [19000, 19003 .. 19200]
+c = M.parallel [c1, c2, c3, c4]
 -- postDur = 380000
 -- bodyDur = 12000000
--- piece = M.seq' [(postDur, M.reverse post), (bodyDur, M.mix [c1, c2, c3, c4]), (postDur, post)]
+piece = M.sequential [c,post]--  [(postDur, M.reverse post), (bodyDur, M.mix [c1, c2, c3, c4]), (postDur, post)]
 -- music = map piece [0 .. postDur * 2 + bodyDur]
-music = M.collapse post
+music = M.collapse piece
 
 main :: IO ()
 main = toWav "test.wav" 44100 music
--- src/Music1Bit/Music.hs
+++ src/Music1Bit/Music.hs
@@ -1,43 +1,59 @@
 module Music1Bit.Music where
 
 type Tick = Bool
+
 type Dur = Int
+
 type IOI = Int
 
-data Primitive = Imp IOI -- | Phasor [IOI] Dur 
+data Primitive = Imp IOI | Phasor [IOI] Dur deriving (Show)
 
-data Music =
-      Prim Primitive 
-    | Music :+: Music -- sequential
-    | Music :=: Music -- parallel
+data Music
+  = Prim Primitive
+  | Music :+: Music -- sequential
+  | Music :=: Music -- parallel
+  deriving (Show)
 
 imp :: IOI -> Music
 imp i = Prim (Imp i)
 
+phasor :: Int -> [IOI] -> Music
+phasor n iois = Prim $ Phasor iois n
+
 sequential :: [Music] -> Music
-sequential (i:is) = i :+: sequential is
 sequential [] = Prim (Imp 0)
+sequential (i : is) = i :+: sequential is
 
-cFold prim seq par m =
-    case m of
-       Prim p -> prim p
-       m1 :+: m2 -> seq (rec m1) (rec m2)
-       m1 :=: m2 -> par (rec m1) (rec m2)
-    where rec = cFold prim seq par
+parallel :: [Music] -> Music
+parallel [] = Prim (Imp 0)
+parallel (i : is) = i :=: parallel is
 
--- -- run a Phasor for n Ticks
--- run :: Int -> [Tick] -> [Tick]
--- run n xs = take n (cycle xs)
+foldMusic prim seq par m =
+  case m of
+    Prim p -> prim p
+    m1 :+: m2 -> seq (rec m1) (rec m2)
+    m1 :=: m2 -> par (rec m1) (rec m2)
+  where
+    rec = foldMusic prim seq par
 
--- collapsePhasor :: [IOI] -> Int -> [Tick]
--- collapsePhasor iois steps = run steps (iois >>= ioi2ticks)
+-- run a Phasor for n Ticks
+run :: Int -> [Tick] -> [Tick]
+run n xs = take n (cycle xs)
 
+collapsePhasor :: [IOI] -> Int -> [Tick]
+collapsePhasor iois steps = run steps (iois >>= ioi2ticks)
 
 ioi2ticks :: IOI -> [Tick]
-ioi2ticks  0 = []
-ioi2ticks  ioi = True : replicate (ioi-1) False
+ioi2ticks 0 = []
+ioi2ticks ioi = True : replicate (ioi - 1) False
 
 collapse :: Music -> [Tick]
 collapse (Prim (Imp ioi)) = ioi2ticks ioi
+collapse (Prim (Phasor iois dur)) = collapsePhasor iois dur -- take dur $ cycle $ concatMap ioi2ticks iois
 collapse (m1 :+: m2) = collapse m1 ++ collapse m2
-collapse (m1 :=: m2) = zipWith (/=) (collapse m1) (collapse m2)
+collapse (m1 :=: m2) =
+  let m1' = collapse m1
+      m2' = collapse m2
+      l = length m1' - length m2'
+      (m1'', m2'') = if l > 0 then (m1', m2' ++ replicate (abs l) False) else (m1' ++ replicate (abs l) False, m2')
+   in zipWith (/=) m1'' m2''

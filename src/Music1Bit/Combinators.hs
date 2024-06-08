c5a67f4ac9073c1880063f030558ea652a8787fe
Víctor Adán <vctradn@gmail.com>
2024-06-08 18:23:24 -0500

    Changed hie.yaml to use cabal. Re-adding tests.

--- src/Music1Bit/Combinators.hs
+++ src/Music1Bit/Combinators.hs
@@ -1,10 +1,10 @@
 module Music1Bit.Combinators where
 
-import           Music1Bit.Types
 
 import           Data.Bits
 import qualified Data.Vector     as V
 
+import           Music1Bit.Types
 
 data Signal = Signal {sample :: Integer -> Tick, dur :: Int}
 
@@ -40,115 +40,13 @@ mix ss = foldr mix2 (silence totalDur) ss
     where
         totalDur = maximum $ map dur ss
 
+-- | Render a Signal into a list of Ticks
 run :: Signal -> [Tick]
-run (Signal f d) = map f [0 .. fromIntegral d]
-
--- train :: V.Vector Tick -> Signal
--- train ticks t d =
---   let
---     len = V.length ticks
---     idx = (len + fromInteger t) `mod` len
---   in Signal (\t -> (ticks V.! idx)) d
+run (Signal f d) = map f [0 .. fromIntegral d - 1]
 
 -- reverse :: Signal a -> Signal a
 -- reverse (Signal f) = Signal $ \t -> f (- t)
 
-
--- seq :: Integer -> Signal a -> Signal a -> Signal a
--- seq d (Signal x) (Signal y) = Signal $ \t -> if t < d then x t else y (t - d)
-
 -- -- | Convolution.
 -- conv :: Integer -> Signal Tick -> Signal Tick -> Signal Tick
 -- conv w (Signal f) (Signal g) = Signal $ \t -> foldr xor False [f (t - m) && g m | m <- [- w .. w]]
-
-
--------------------------------------------------------------
--- import Data.Bits
--- import Data.MemoTrie
--- import qualified Data.Vector.Unboxed as V
-
--- import Music1Bit.Types
-
--- type Time       = Integer
--- type Signal = Time -> Maybe Tick
-
--- -- type Signal2 a = () -> (a, Signal2)
-
--- -- import Data.List.NonEmpty
-
--- -- | Constant 0.
--- silence :: Signal
--- silence = const $ Just False
-
--- train :: V.Vector Tick -> Signal
--- train ticks t d =
---   let
---     len = V.length ticks
---     idx = (len + fromInteger t) `mod` len
---   in Signal (\t -> (ticks V.! idx)) d
-
--- ioi2signal :: [IOI] -> Signal
--- ioi2signal iois = train $ V.fromList $ iois2ticks iois
-
--- iois2ticks :: [IOI] -> [Tick]
--- iois2ticks = concatMap ioi2ticks
-
--- ioi2ticks :: IOI -> [Tick]
--- ioi2ticks ioi
---   | ioi == 0 = []
---   | otherwise = True : replicate (ioi -1) False
-
--- -- -- | Tick-ish
--- -- impulse :: IOI -> Signal
--- -- impulse ioi t = (ioi + t) `mod` ioi == 0
--- --
--- -- impulseM = memo impulse
--- --
--- -- shift :: Integer -> Signal -> Signal
--- -- shift int s t = s (t + int)
--- --
--- -- -- | Mix two signals together by adding amplitudes.
--- -- add :: Signal -> Signal -> Signal
--- -- add x y t = x t `xor` y t
--- --
--- -- -- | Mix signals together by adding amplitudes.
--- -- mix :: [Signal] -> Signal
--- -- mix = foldr add silence
--- --
--- -- seq :: (Signal, Integer) -> Signal -> Signal
--- -- seq (x, d) y t = if t < d then x t else y (t - d)
--- --
--- -- seq' :: [(Integer, Signal)] -> Signal
--- -- seq' ss = train $ V.fromList $ concatMap run ss
--- --   where
--- --     run :: (Integer, Signal) -> [Tick]
--- --     run (d, s) = map s [0 .. d]
--- --
--- -- inverse :: Signal -> Signal
--- -- inverse x t = not $ x t
--- --
--- -- reverse :: Signal -> Signal
--- -- reverse s t = s (-t)
--- --
--- -- fib n = fibs !! n
--- --   where
--- --     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
--- --
--- -- -- fm :: ModSignal -> Signal -> Signal
--- -- -- fm x y t = y (t + x t)
--- --
--- -- -- | Integrate
--- -- toInt :: Signal -> (Integer -> Integer)
--- -- toInt s t = if s t then 1 else 0
--- --
--- -- fromInt :: (Integer -> Integer) -> Signal
--- -- fromInt s t = s t /= 0
--- --
--- -- -- | Calculate an oscillation frequency for a MIDI note number, in an equally tempered scale.
--- -- midiNoteToFreq :: (Floating a) => Int -> a
--- -- midiNoteToFreq n =
--- --   f0 * (a ** (fromIntegral n - midiA4))
--- --   where
--- --     a = 2 ** (1.0 / 12.0)
--- --     f0 = 440.0 -- A-4 in an ETS is 440 Hz.
--- --     midiA4 = 69 -- A-4 in MIDI is 69.
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,50 +1,22 @@
-import Test.Tasty
-import Test.Tasty.HUnit
-import Test.Tasty.QuickCheck as QC
+module Main where
 
-import Music1Bit.Comp (run)
-import Music1Bit.Tick (Tick(..))
-import Music1Bit.Boolean ((&), (\&), (#))
+import           Test.Hspec
+import           Test.QuickCheck
+
+import           Music1Bit.Combinators
+
 
 main :: IO ()
-main = defaultMain unitTests
-
-unitTests :: TestTree
-unitTests = testGroup "Tests" [runTest, tickTest]
-
-{-propsTests :: TestTree-}
-{-propsTests = testGroup "Properties" [runProps]-}
-
-{-runProps = testGroup "run length"-}
-  {-[-}
-    {-QC.testProperty "run length" $-}
-    {-\n xs -> length (run (n::Int) (xs::[Tick])) == n-}
-  {-]-}
-
-runTest = testGroup "run Phasor"
-  [ testCase "run n < length" $
-    run 2 [I,O,O]  @?= [I,O]
-
-  , testCase "run n > length" $
-    run 6 [I,O,O] @?= [I,O,O,I,O,O]
-
-  , testCase "run n == 0" $
-    run 0 [I,O,O] @?= []
-
-  ]
-
-tickTest = testGroup "a ++ b"
-  [ testCase "[I,O] ++ [I]" $
-    [I,O] ++ [I] @?= [I,O,I]
-
-  , testCase "a & b" $
-    ([I,I,O,O] & [I,O,I,O]) @?= [I,O,O,O]
-
-  , testCase "a \\& b" $
-    ([I,I,O,O] \& [I,O,I,O]) @?= [I,I,I,O]
-
-  , testCase "a # b" $
-    ([I,I,O,O] # [I,O,I,O]) @?= [O,I,I,O]
-  ]
-
-
+main = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
+    it "2 + 2 is equal to 4" $ do
+      2 + 2 `shouldBe` 4
+    it "x + 1 is always greater than x" $ do
+      property $ \x -> x + 1 > (x :: Int)
+  describe "run" $ do
+    it "run returns the correct number of values" $ do
+      run (silence 1) `shouldBe` [False]
+      run (silence 0) `shouldBe` []
+      run (silence (-1)) `shouldBe` []

35f4c5b158df38d3d1b0826fbe181b6c5cea4a84
Víctor Adán <vctradn@gmail.com>
2024-09-11 22:09:24 -0500

    small improvement to tepoz x

--- compositions/tepoz_x/Main.hs
+++ compositions/tepoz_x/Main.hs
@@ -7,11 +7,12 @@ import           Music1Bit.Combinators as C
 import           Music1Bit.Music       as M
 
 
-count = 9000000
-freq = 450
+count = 1000000
+-- count = 9000000
+freq = 410
 
 pre1 = M.train (reverse [100, 200 .. 8001]) [(True, False)]
-pre2 = M.train (reverse [102, 200 .. 8000]) [(False, True)]
+pre2 = M.train (reverse [102, 200 .. 8000]) [(True, True)]
 pre = M.ormix [pre1, pre2]
 
 post1 = M.train [120, 200 .. 7002] [(False, True)]
@@ -27,8 +28,8 @@ p2c = M.train (replicate (count `div` freq `div` 3) (freq + 1)) [(False, True)]
 -- p2c = M.phasor (count `div` 3) [freq + 1] [(False, True)]
 p2 = M.sequential [p2a, p2b, p2c]
 
-noise1 = M.phasor count (700000 : take 600 (randomRs (2,7) (mkStdGen 2))) [(False, True), (False, True), (True, True)]
-noise2 = M.phasor count (500011 : take 100 (randomRs (4,21) (mkStdGen 1))) [(True, False), (True, False), (True, True)]
+noise1 = M.phasor count (700000 : take 181 (randomRs (2,7) (mkStdGen 2))) [(False, True), (False, True), (True, True)]
+noise2 = M.phasor count (500011 : take 100 (randomRs (7,31) (mkStdGen 1))) [(True, False), (True, False), (True, True)]
 
 music = M.sequential [pre, M.xormix [p1, p2, noise1, noise2], post]
 

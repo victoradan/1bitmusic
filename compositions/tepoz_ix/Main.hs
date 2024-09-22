108eb82377b436e0f40d4cce64f39c9b9a8b066b
Víctor Adán <vctradn@gmail.com>
2024-09-22 11:05:26 -0500

    tepoz_ix pre and post are stereo

--- compositions/tepoz_ix/Main.hs
+++ compositions/tepoz_ix/Main.hs
@@ -38,26 +38,30 @@ down = N Down
 down2 = down :. same
 
 
-r1a = Rule harm up
-r1b = Rule same down
-r1c = Rule harm Main.seq
-r1d = Rule Main.seq same
-r1e = Rule up Main.seq
-r1f = Rule down harm
-r1g = Rule down2 up
-r1h = Rule harm down2
-r1i = Rule Main.seq down
-r1j = Rule sc down2
-r1k = Rule same sc
+ra = Rule harm up
+rb = Rule same down
+rc = Rule harm Main.seq
+rd = Rule Main.seq same
+re = Rule up Main.seq
+rf = Rule down harm
+rg = Rule down2 up
+rh = Rule harm down2
+ri = Rule Main.seq down
+rj = Rule sc down2
+rk = Rule same sc
 
-g1 = Grammar down (Uni [r1a, r1b, r1c, r1d, r1e, r1f, r1g, r1h, r1i, r1j, r1k ])
+g1 = Grammar down (Uni [ra, rb, rc, rd, re, rf, rg, rh, ri, rj, rk ])
 
 t1 n = interpret (L.gen L.replFun g1 3 !! n) ir (M.phasor 400 [300, 400] [(True, False)])
 
-pre = M.train (reverse [20, 200 .. 13001]) [(True, True)]
-post = M.train [3, 120 .. 7002] [(True, True)]
+pre1 = M.train (reverse [20, 200 .. 13001]) [(False, True)]
+pre2 = M.train (reverse [21, 200 .. 13002]) [(True, False)]
+pre = M.ormix [pre1, pre2]
+post1 = M.train [3, 120 .. 7002] [(True, False)]
+post2 = M.train [4, 120 .. 7001] [(False, True)]
+post = M.ormix [post1, post2]
 
 music = M.sequential [pre, t1 17, post]
 
 main :: IO ()
-main = toWav2 "test.wav" 44100 $ C.run $ M.collapse music
+main = toWav2 "tepoz_ix.wav" 44100 $ C.run $ M.collapse music
